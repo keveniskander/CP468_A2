c35b9c9c302eab0ba1373e4a516ea77a3db4f692
keveniskander <keven.iskander@hotmail.com>
2020-11-02 22:14:22 -0500

    Initial commit

--- sudoku.py
+++ sudoku.py
@@ -9,3 +9,17 @@ Authors:  Keven Iskander, Carla Castaneda, Nicole Laslavic, Alexander Francis
 __updated__ = "2020-11-02"
 -------------------------------------------------------
 """
+
+class Sudoku:
+
+    def __init__(self):
+        self.table = [['hey carla, UwU']for i in range(8)]
+
+def main():
+    sud = Sudoku()
+    print(sud.table)
+
+    
+
+if __name__ == "__main__":
+    main()
