bcbf89182ba170b80f660e1dde35af910464bafb
keveniskander <keven.iskander@hotmail.com>
2020-11-02 22:48:10 -0500

    print function added

--- sudoku.py
+++ sudoku.py
@@ -13,13 +13,26 @@ __updated__ = "2020-11-02"
 class Sudoku:
 
     def __init__(self):
-        self.table = [['hey carla, UwU']for i in range(8)]
+        self.table = [[1 for i in range(9)] for j in range(9)]
+
+
+    # Code taken from https://stackoverflow.com/questions/37952851/formating-sudoku-grids-python-3
+    def print_table(self):
+        print("-"*37)
+        
+        for i, row in enumerate(self.table):
+            print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
+            if i == 8:
+                print("-"*37)
+            elif i % 3 == 2:
+                print("|" + "---+"*8 + "---|")
+            else:
+                print("|" + "   +"*8 + "   |")
+
 
 def main():
     sud = Sudoku()
-    print(sud.table)
-
-    
+    sud.print_table()
 
 if __name__ == "__main__":
     main()
