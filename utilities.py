07382ec12a2b84fc3aa16a0fee0c2b23fecc36a0
keveniskander <59800096+keveniskander@users.noreply.github.com>
2021-01-13 21:49:09 -0500

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -7,49 +7,58 @@ CP468
 Assignment 2
 Authors:  Keven Iskander, Carla Castaneda, Nicole Laslavic, Alexander Francis
 __updated__ = "2020-11-02"
+Queue implementation from CP164
 -------------------------------------------------------
 """
 from copy import deepcopy
 
 
-class PriorityQueue:
-
+class Queue:
     def __init__(self):
         """
         -------------------------------------------------------
-        Initializes an empty priority queue.
-        Use: pq = PriorityQueue()
+        Initializes an empty queue. Data is stored in a list.
+        Use: q = Queue()
         -------------------------------------------------------
         Postconditions:
-            Initializes an empty priority queue.
+            Initializes an empty queue.
         -------------------------------------------------------
         """
         self._values = []
-        self._first = None
         return
 
     def is_empty(self):
         """
         -------------------------------------------------------
-        Determines if the priority queue is empty.
-        Use: b = pq.is_empty()
+        Determines if the queue is empty.
+        Use: b = q.is_empty()
         -------------------------------------------------------
         Postconditions:
-            returns
-            True if priority queue is empty, False otherwise.
+            Returns True if the queue is empty, False otherwise.
         -------------------------------------------------------
         """
         return len(self._values) == 0
 
+    def is_full(self):
+        """
+        -------------------------------------------------------
+        Determines if the queue is full.
+        Use: b = q.is_full()
+        -------------------------------------------------------
+        Postconditions:
+            Returns True if the queue is full, False otherwise.
+        -------------------------------------------------------
+        """
+        return False
+
     def __len__(self):
         """
         -------------------------------------------------------
-        Returns the length of the priority queue.
+        Returns the length of the queue.
         Use: n = len(q)
         -------------------------------------------------------
         Postconditions:
-            returns
-            the number of values in the priority queue.
+            Returns the number of values in the queue.
         -------------------------------------------------------
         """
         return len(self._values)
@@ -57,132 +66,61 @@ class PriorityQueue:
     def insert(self, value):
         """
         -------------------------------------------------------
-        Inserts a copy of value into the priority queue.
-        Use: pq.insert(value)
+        Inserts a copy of value into the queue.
+        Use: q.insert(value)
         -------------------------------------------------------
         Preconditions:
             value - a data element (?)
         Postconditions:
-            a copy of value is added to the priority queue.
+            a copy of value is added to the rear of the queue.
         -------------------------------------------------------
         """
         self._values.append(deepcopy(value))
-
-        if self._first == None:
-            self._first = 0
-        elif value<self._values[self._first]: 
-            self._first = len(self._values)-1
-
         return
 
     def remove(self):
         """
         -------------------------------------------------------
-        Removes and returns value from the priority queue.
-        Use: v = pq.remove()
+        Removes and returns value from the queue.
+        Use: v = q.remove()
         -------------------------------------------------------
         Postconditions:
             returns
-            value - the highest priority value in the priority queue -
-            the value is removed from the priority queue. (?)
+            value - the value at the front of the queue - the value is
+            removed from the queue (?)
         -------------------------------------------------------
         """
-        assert len(
-            self._values) > 0, "Cannot remove from an empty priority queue"
- 
-        value = self._values.pop(self._first)
-        
-        i = 0
-        self._first = 0
-        while i<len(self._values):
-            if self._values[self._first]>self._values[i]:
-                self._first = i
-            i+=1
-
+        assert len(self._values) > 0, "Cannot remove from an empty queue"
+        value=self._values.pop(0)
         return value
 
     def peek(self):
         """
         -------------------------------------------------------
-        Peeks at the highest priority value of the priority queue.
-        Use: v = pq.peek()
+        Peeks at the front of queue.
+        Use: v = q.peek()
         -------------------------------------------------------
         Postconditions:
             returns
-            value - a copy of the highest priority value in the priority queue -
-            the value is not removed from the priority queue. (?)
+            value - a copy of the value at the front of the queue -
+            the value is not removed from the queue (?)
         -------------------------------------------------------
         """
-        assert len(self._values) > 0, "Cannot peek at an empty priority queue"
-
-        value = deepcopy(self._values[self._first])
-
+        assert len(self._values) > 0, "Cannot peek at an empty queue"
+        value = deepcopy(self._values[0])
         return value
-
-    def split(self):
-        """
-        -------------------------------------------------------
-        Splits a priority queue into two with values going to 
-                alternating priority queues. The split is stable.
-        Use: pq2, pq3 = pq1.split()
-        -------------------------------------------------------
-        Postconditions:
-            returns
-            pq2 - a priority queue that contains alternating values
-                from the current queue (PriorityQueue)
-            pq3 - priority queue that contains  alternating values
-                from the current queue  (PriorityQueue)
-            The current priority queue is empty
-        -------------------------------------------------------
-        """
-        pq2 = PriorityQueue()
-        pq3 = PriorityQueue()
-        while self.is_empty()==False:
-            pq2.insert(self.remove())
-            
-            if self.is_empty()==False:
-                pq3.insert(self.remove())
-                
-
-        return pq2, pq3
-
-    def combine(self, pq2):
-        """
-        -------------------------------------------------------
-        Combines contents of two priority queues into a new 
-        priority queue.
-        Use: pq3 = pq1.combine(pq2)
-        -------------------------------------------------------
-        Preconditions:
-            q2 - an array-based queue (Queue)
-        Postconditions:
-            returns
-            pq3 - Contents of self and q2 are interlaced 
-                into pq3 (Queue)
-        -------------------------------------------------------
-        """
-        
-        pq3 = PriorityQueue()
-        
-        
-        while self.is_empty()==False:
-            pq3.insert(self.remove())
-            if pq2.is_empty()==False:
-                pq3.insert(pq2.remove())
-            
-        return pq3
 
     def __iter__(self):
         """
         FOR TESTING ONLY
         -------------------------------------------------------
-        Generates a Python iterator. Iterates through the priority queue
-        from front to rear. Not in priority order.
+        Generates a Python iterator. Iterates through the queue
+        from front to rear.
         Use: for v in q:
         -------------------------------------------------------
         Postconditions:
             returns
-            value - the next value in the priority queue (?)
+            value - the next value in the queue (?)
         -------------------------------------------------------
         """
         for value in self._values:
